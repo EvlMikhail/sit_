ba9fdd4a239dd24feddf9d4ed77cf8d155dc92f3
RuLauf <dolgov.pavel007@gmail.com>
2025-06-01 21:54:29 +0400

    Исправлено начало последовательности

--- task.cpp
+++ task.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 int lib(int n) {
-    if (n == 0 || n == 1) return 1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
     return lib(n-1) + lib(n-2);
 }
 
 int main() {
-    std::cout << lib(10) << std::endl; // Выведет 55
+    std::cout << lib(10) << std::endl; // Выведет 34
     return 0;
 }
