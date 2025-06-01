092a02beed40753d504eaf328cef079281e202a9
RuLauf <dolgov.pavel007@gmail.com>
2025-06-01 21:53:58 +0400

    Добавлена рекурсивная функция Фибоначчи

--- task.cpp
+++ task.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int lib(int n) {
+    if (n == 0 || n == 1) return 1;
+    return lib(n-1) + lib(n-2);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << lib(10) << std::endl; // Выведет 55
     return 0;
 }
